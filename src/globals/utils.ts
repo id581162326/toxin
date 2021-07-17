263c4cc904d333dc2e3479779cc33e2d8d9ca72a
id581162326 <starbro85@gmail.com>
2021-07-17 19:39:13 +0700

    Update. Add utils.

---
+++ src/globals/utils.ts
@@ -0,0 +1,10 @@
+export type Plural = {one: string, few: string, many: string};
+
+export const plurals: Record<string, Plural> = {
+  'adults': {one: 'Взрослый', few: 'Взрослых', many: 'Взрослых' },
+  'children': {one: 'Ребенок', few: 'Детей', many: 'Детей' },
+  'babies': {one: 'Младенец', few: 'Младенцев', many: 'Младенцев' },
+  'rooms': {one: 'Комната', few: 'Комнаты', many: 'Комнат' },
+  'beds': {one: 'Кровать', few: 'Кровати', many: 'Кроватей' },
+  'bathrooms': {one: 'Ванная комната', few: 'Ванных камнаты', many: 'Ванных комнат' }
+}
